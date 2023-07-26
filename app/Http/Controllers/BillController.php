4cd747d19e2d9200d0303c7b973eaa0e8eb8d2f7
មុនី​ រតន៍​ <dr.munyroth@gmail.com>
2023-07-26 11:43:21 +0700

    fifteenth commit

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bill;
-use App\Http\Requests\StoreBillRequest;
-use App\Http\Requests\UpdateBillRequest;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,23 +23,6 @@ class BillController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreBillRequest $request, Bill $bill): Response
-    {
-        $userId =  Auth::guard('api')->user()['id'];
-
-        $bill->user_id = $userId;
-        $bill->save();
-
-        return Response([
-            'status' => 201,
-            'message' => 'uploaded successfully',
-            'data' => $bill
-        ], 201);
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(Bill $bill): Response
@@ -51,21 +32,5 @@ class BillController extends Controller
             'message' => 'got successfully',
             'data' => $bill->load('billDetails')
         ], 200);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateBillRequest $request, Bill $bill)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Bill $bill)
-    {
-        //
     }
 }
